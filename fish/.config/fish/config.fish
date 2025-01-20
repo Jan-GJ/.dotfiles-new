b98a963afa8e95d9a976aa26117f418ec695f1d5
Jan GJ <gameforsz@gmail.com>
2025-01-20 12:02:52 +0100

    feat: added fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+	# Commands to run in interactive sessions can go here
+end
+
+# theme
+if test -f ~/.config/fish/tokyonight_night.fish
+    source ~/.config/fish/tokyonight_night.fish
+end
+
+# eza 'ls' replacement
+alias lse="eza -a -l --absolute --header --icons --git -s=type"
+alias ls="lse --no-user --no-permissions --no-filesize --no-time --absolute=off"
+
+# nvim
+alias nv="nvim"
